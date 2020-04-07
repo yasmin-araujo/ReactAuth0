880929d59e4891b1f6cd13214ee0c34080bb50fd
yasmin_araujo <yasminoa@hotmail.com>
2020-04-07 18:15:25 -0300

    Logout, Signup, and User Profile
    - Implement Logout
    - Review Signup Process
    - Get User Profile
    - Configure Profile Page Route
    - Display User Profile

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import {Route} from  'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Home from './Home';
 import Profile from './Profile';
 import Nav from './Nav';
-import Callback from './Callback';
 import Auth from './Auth/Auth';
+import Callback from './Callback';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.auth = new Auth(this.props.history);
-    }
+	constructor(props) {
+		super(props);
+		this.auth = new Auth(this.props.history);
+	}
 	render() {
-        return (
-            <>
-                <Nav />
-                <div className="body">
-                    <Route path="/" exact render={props => <Home auth={this.auth} {...props} />} />
-                    <Route path="/callback" render={props => <Callback auth={this.auth} {...props} />} />
-                    <Route path="/profile" component={Profile} />
-                </div>
-            </>
-        );
+		return (
+			<div>
+				<Nav auth={this.auth} />
+				<div className="body">
+					<Route path="/" exact render={(props) => <Home auth={this.auth} {...props} />} />
+					<Route path="/callback" render={(props) => <Callback auth={this.auth} {...props} />} />
+					<Route
+						path="/profile"
+						render={(props) =>
+							this.auth.isAuthenticated() ? <Profile auth={this.auth} {...props} /> : <Redirect to="/" />}
+					/>
+				</div>
+			</div>
+		);
 	}
 }
 
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,7 +2,8 @@ import auth0 from 'auth0-js';
 
 export default class Auth {
 	constructor(history) {
-		this.history = history;
+        this.history = history;
+        this.userProfile = null;
 		this.auth0 = new auth0.WebAuth({
 			domain: process.env.REACT_APP_AUTH0_DOMAIN,
 			clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
@@ -27,12 +28,12 @@ export default class Auth {
 				this.history.push('/');
 				alert(`Error: ${err.error}. Check the console for further details. `);
 				console.log(err);
-			} else console.log('ue');
+			}
 		});
 	};
 
 	setSession = (authResult) => {
-		//set the time that the acess token will expire
+		// set the time that the access token will expire
 		// seconds * 1000(to converto into miliseconds)
 		const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
 
@@ -46,4 +47,34 @@ export default class Auth {
 		const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
 		return new Date().getTime() < expiresAt;
 	}
+
+	logout = () => {
+		// Logging the user out locally
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('id_token');
+		localStorage.removeItem('expires_at');
+
+		this.userProfile = null;
+
+		this.auth0.logout({
+			clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+			returnTo: 'http://localhost:3000'
+		});
+	};
+
+	getAccessToken = () => {
+		const accessToken = localStorage.getItem('access_token');
+		if (!accessToken) {
+			throw new Error('No access token found.');
+		}
+		return accessToken;
+	};
+
+	getProfile = (cb) => {
+		if (this.userProfile) return cb(this.history.userProfile);
+		this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+			if (profile) this.userProfile = profile;
+			cb(profile, err);
+		});
+	};
 }
